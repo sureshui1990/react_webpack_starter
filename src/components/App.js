84e5bdc71530dfd190d367c318db37592f5dd219
suresh bethanasamy <suresh.b@WSEINML-GSRZFVH.local>
2019-09-23 15:14:39 +0530

    Adding simple boilerplate for reactjs startup - webpack config

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='text-center'>
+                <h1>React App</h1>
+
+                <p>Start the fun with ReactJs</p>
+                
+            </div>
+        )
+    }
+}
+
+export default App;
